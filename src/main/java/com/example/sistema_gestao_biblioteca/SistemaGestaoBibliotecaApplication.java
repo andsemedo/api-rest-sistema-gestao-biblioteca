727b7f4661c09127e4c50567bbefe80b954f25af
Anderson Semedo <andsemedo023@gmail.com>
2023-12-15 17:02:12 -0100

    dependencia do swagger ui adicionado

--- src/main/java/com/example/sistema_gestao_biblioteca/SistemaGestaoBibliotecaApplication.java
+++ src/main/java/com/example/sistema_gestao_biblioteca/SistemaGestaoBibliotecaApplication.java
@@ -1,9 +1,12 @@
 package com.example.sistema_gestao_biblioteca;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Biblioteca API", version = "2.0", description = "Sistema de Gestão de Biblioteca"))
 public class SistemaGestaoBibliotecaApplication {
 
 	public static void main(String[] args) {
